f068b862c162b790e21fd94267e4d2ce34920717
Sam Tucker <samueljeffersontucker@gmail.com>
2014-03-20 12:19:44 -0500

    chat

--- app/static/js/source/main.js
+++ app/static/js/source/main.js
@@ -1,3 +1,5 @@
+/* global io:true */
+
 (function(){
 
   'use strict';
@@ -6,7 +8,30 @@
 
   function initialize(){
     $(document).foundation();
+    initializeSocketIO();
+    $('button').click(sendMessage);
   }
 
-})();
+  var socket;
 
+  function initializeSocketIO(){
+    socket = io.connect('/app');
+    socket.on('online', function(data){console.log(data)});
+    socket.on('message', addMessage);
+  }
+
+  function sendMessage(){
+    var data = {};
+    data.text = $('textarea').val();
+    socket.emit('newmessage', data);
+  }
+
+  function addMessage(data){
+    console.log('Received message from node: ')
+    console.log(data);
+    var $message = $('<div>'+data.text+'</div>');
+    $('textarea').prepend($message);
+  }
+
+
+})();
